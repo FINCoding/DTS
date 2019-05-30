568fc6fc2c6232e04e148c9fbef1ff311bf29aea
FIN <nasibullin_if@taifnk.ru>
2019-05-30 13:52:07 +0300

    '300519'

--- db_auto_upd.go
+++ db_auto_upd.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "os"
     "io"
+    "bufio"
 
     "github.com/DTS/models"
     "github.com/jinzhu/gorm"
@@ -12,11 +13,17 @@ import (
 )
 
 var db *gorm.DB
+var err error
+var auto []models.Auto
 
 func main() {
     //readfile()
-    upd_db_tst()
+    readfile2()
+    //upd_db_tst()
+    //tst_upd_db()
 }
+
+
 
 func print_err(err error) {
     if err != nil{
@@ -25,16 +32,27 @@ func print_err(err error) {
     }
 }
 
-
 func upd_db_tst() {
-    auto := models.Auto{Brand: "Lada", Model:"Niva"}
+    db, err = gorm.Open("sqlite3", "./gorm.db")
+    if err != nil {
+       fmt.Println(err)
+    }
+    defer db.Close()
 
-    fmt.Print(db.NewRecord(auto))
-    db.Create(&auto)
+    //auto1 := models.Auto{Brand:"Lada", Model:"Niva"}
+    auto := models.Auto{}
+    fmt.Println(db.First(&auto))
+
+    /*fmt.Print(db.NewRecord(auto))
+    db.Create(&auto1)
     fmt.Print(db.NewRecord(auto))
 
+    fmt.Println(db.Where("model = ?", "Niva").First(&auto))*/
+    //get_all_enries(db)
 
 }
+
+
 func readfile() {
     file, err := os.Open("./data_from_parsing_files/lada.txt")
     print_err(err)
@@ -48,6 +66,74 @@ func readfile() {
         if err == io.EOF{
             break
         }
-        fmt.Println(string(data[:n]))
+        item_auto := models.Auto{Brand: "Lada", Model:string(data[n])}
+        fmt.Println("item_auto ", item_auto)
+        fmt.Println("string(data[:n]) ", string(data[:n]))
+        auto = append(auto, item_auto)
+        //fmt.Println(item_auto)
+        //fmt.Println(string(data[:n]))
     }
+    //fmt.Println(auto)
+}
+
+
+func tst_upd_db() {
+
+    type Document struct{
+        gorm.Model
+        Body string
+    }
+
+    db, err = gorm.Open("sqlite3", "./gorm.db")
+    if err != nil {
+       fmt.Println(err)
+    }
+    defer db.Close()
+
+    //tst_doc := Document {
+    //    Body: "This is a test document",
+    //}
+
+    //db.AutoMigrate(&Document{})
+    //fmt.Println(db.HasTable(&Document{}))
+
+    //db.Create(&tst_doc)
+   // var doc []Document
+
+    if err := db.Find(&auto).Error; err != nil {
+        fmt.Println(err)
+    } else {
+        for i,val := range(auto) {
+            fmt.Println(i, val)
+        }
+
+    }
+}
+
+func readfile2() {
+    file, err := os.Open("./data_from_parsing_files/lada.txt")
+    print_err(err)
+    defer file.Close()
+
+    db, err = gorm.Open("sqlite3", "./gorm.db")
+    if err != nil {
+       fmt.Println(err)
+    }
+    defer db.Close()
+
+    reader := bufio.NewReader(file)
+
+    for{
+        line, _, err := reader.ReadLine()
+
+        if err == io.EOF{
+            break
+        }
+
+        item_auto := models.Auto{Brand: "Lada", Model:string(line)}
+        auto = append(auto, item_auto)
+        db.Create(&auto)
+    }
+
+    //fmt.Println(db.Find(&models.Auto{}))
 }
--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
     "net/http"
     "html/template"
 	"fmt"
+	"os"
 
 	"github.com/DTS/models"
     "github.com/go-martini/martini"
-    "github.com/martini-contrib/render"
+    //"github.com/martini-contrib/render"
     "github.com/jinzhu/gorm"
      _ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -17,31 +18,32 @@ var err error
 var discounts map[string]*models.Discount
 
 func main() {
+    fmt.Println(os.Getenv("GOPATH"))
     m := martini.Classic()
     CreateDBTable()
 
-    m.Use(render.Renderer(render.Options{
+   /* m.Use(render.Renderer(render.Options{
 		Directory:  "templates",                         // Specify what path to load the templates from.
 		Layout:     "layout",                            // Specify a layout template. Layouts can call {{ yield }} to render the current template.
 		Extensions: []string{".tmpl", ".html"},          // Specify extensions to load for templates.
-		Funcs:      []template.FuncMap{unescapeFuncMap}, // Specify helper function maps for templates to access.
+		//Funcs:      []template.FuncMap{},             // Specify helper function maps for templates to access.
 		Charset:    "UTF-8",                             // Sets encoding for json and html content-types. Default is "UTF-8".
 		IndentJSON: true,                                // Output human readable JSON
-	}))
+	}))*/
 
     staticOptions := martini.StaticOptions{Prefix:"static"}
     m.Use(martini.Static("static", staticOptions))
     m.Get("/", indexHandler)
     m.Get("/write", writeHandler)
-    m.Get("/addsc", addServiceCentre)
+   // m.Get("/addsc", addServiceCentre)
     m.Run()
 }
 
 
 
-func addServiceCentre() {
+/*func addServiceCentre() {
 
-}
+}*/
 
 func CreateDBTable() {
    db, err = gorm.Open("sqlite3", "./gorm.db")
@@ -52,6 +54,7 @@ func CreateDBTable() {
     db.AutoMigrate(&models.Discount{}, &models.Service_centre{}, &models.Auto{})
     fmt.Print("it's", db.HasTable(&models.Discount{}))
 }
+
 func indexHandler(w http.ResponseWriter, r *http.Request){
     t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
     if err != nil{
